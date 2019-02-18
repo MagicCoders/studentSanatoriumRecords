d8ad063bf282dba17e58a3ac8ec66cc601dac3c4
Kijedi <wkijedi@gmail.com>
2019-02-18 15:42:00 +0300

    students table dummy data

---
+++ students.sql
@@ -0,0 +1,13 @@
+INSERT INTO `students` (`patientID`, `regNo`, `fname`, `sname`, `gender`, `YOB`) VALUES 
+        ('EUSAN003', 'S13/09718/15', 'christine', 'peter', 'female', '2019-02-07'), 
+        ('EUSAN004', 'S13/09719/15', 'sali', 'gaki', 'female', '1998-02-05'),
+        ('EUSAN012', 'S13/09710/15', 'erick', 'ogaro', 'male', '1999-02-07'), 
+        ('EUSAN014', 'S13/09711/15', 'roy', 'mwema', 'male', '1998-02-07'), 
+        ('EUSAN005', 'S13/09712/15', 'mary', 'kipchumba', 'female', '1995-02-07'), 
+        ('EUSAN006', 'S13/09713/15', 'diana', 'chebet', 'female', '1999-02-07'), 
+        ('EUSAN007', 'S13/09714/15', 'mellisa', 'kamau', 'female', '2000-02-07'), 
+        ('EUSAN008', 'S13/09715/15', 'ken', 'kibet', 'male', '1990-02-07'), 
+        ('EUSAN009', 'S13/09716/15', 'mercy', 'okoreh', 'female', '1993-02-07'), 
+        ('EUSAN010', 'S13/09717/15', 'carol', 'njoki', 'female', '1993-02-07'), 
+        ('EUSAN011', 'S13/09743/15', 'frank', 'opiyo', 'male', '1990-02-07'): 
+
